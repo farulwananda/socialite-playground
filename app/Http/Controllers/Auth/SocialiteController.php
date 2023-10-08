cfcd1387e0cbdb153dab5aab3318c584eee15d9a
Farul Wananda <farulahmad2002@gmail.com>
2023-10-08 09:30:59 +0700

    feat: add google auth avatar

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends Controller
@@ -22,12 +23,19 @@ class SocialiteController extends Controller
         $existingUser = User::where('email', $user->getEmail())->first();
 
         if ($existingUser) {
+            if ($existingUser->name !== $user->getName() || $existingUser->image !== $user->getAvatar()) {
+                $existingUser->name = $user->getName();
+                $existingUser->image = $user->getAvatar();
+                $existingUser->save();
+            }
+
             Auth::login($existingUser, true);
         } else {
             $newUser = User::create([
                 'name' => $user->getName(),
                 'email' => $user->getEmail(),
                 'password' => bcrypt('password'),
+                'image' => $user->getAvatar(),
             ]);
 
             Auth::login($newUser, true);
